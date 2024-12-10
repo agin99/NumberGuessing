6c7f9d61c3bdc5740ae135975326024dcc3902ef
agin99 <alan.gindi99@gmail.com>
2024-12-10 12:56:21 +0100

    refactoring and implementing proper server shutdown

--- game_server_startup.py
+++ game_server_startup.py
@@ -1,42 +1,48 @@
 import socket
 
-def start_server(IPv4, port):
+def handle_client(client_directory, clientID, client_socket, client_address):
+    print(f"Connected to {client_address}")
+    client_socket.send(f"Your clientID is {clientID}.".encode('utf-8'))
+    #Handle player action
+    try: 
+        while True:
+            msg = client_socket.recv(1024).decode('utf-8')
+            if msg == 'exit':
+                print(f"Client {clientID} disconnected.")
+                break
+            print(f"Client {clientID}: {msg}")
+    finally:
+        client_socket.close()
+        del client_directory[f'{clientID}'] 
+        print(f"Connection to {clientID} closed.")
+    return client_directory
+
+def main():
+    #Start server
     clientID = 0
     client_directory = {}
-
-    #Start server
+    IPv4 = '127.0.0.1'
+    port = 8080
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((IPv4, port))
     server_socket.listen(1)
     print(f"Listening on {IPv4}:{port}...")
 
-    #Handle client connection
-    while True:
-        print("Searching for client connection...")
-        client_socket, client_address = server_socket.accept()
-        print(f"Connected to {client_address}")
-        client_socket.send(f"Your clientID is {clientID}.".encode('utf-8'))
-        client_directory[f'{clientID}'] = {
-            'socket' : client_socket,
-            'address' : client_address
-        }
-        clientID += 1
-        #Handle player action
-        try: 
-            while True:
-                msg = client_socket.recv(1024).decode('utf-8')
-                if msg == 'exit':
-                    print(f"Client {clientID} disconnected.")
-                    break
-                print(f"Client {clientID}: {msg}")
-        finally:
-            client_socket.close()
-            for key in client_directory:
-                if client_directory[key]['socket'] == client_socket:
-                    clientID = int(key)
-                    break
-            del client_directory[f'{clientID}']
-            print(f"Connection to {clientID} closed.")
+    try:
+        while True:
+            print("Searching for client connection...")
+            client_socket, client_address = server_socket.accept()
+            client_directory[f'{clientID}'] = {
+                'socket' : client_socket,
+                'address' : client_address
+            }
+            handle_client(client_directory, clientID, client_socket, client_address)
             clientID += 1
+    except KeyboardInterrupt:
+        print("\nServer shutting down...")
+        
+    finally:
+        server_socket.close()
+        print("Server fully shut down.")
 
-start_server('127.0.0.1', 8080)
+main()
--- player_client.py
+++ player_client.py
@@ -11,21 +11,25 @@ def main():
         #Connect player to game server
         server_IPv4 = input("Submit the server IPv4: ")
         server_port = int(input("Submit the server port: "))
-        client_socket = connect_client_to_game(server_IPv4, server_port)
+        game_connection = connect_client_to_game(server_IPv4, server_port)
 
         #Send messages to game server
         while True:
             msg = input("What would you like to say to the server? \n")
             if not msg:
                 print("Closing the connection to the server.")
-                client_socket.send('exit'.encode('utf-8'))
+                game_connection.send('exit'.encode('utf-8'))
                 break
-            client_socket.send(f"{msg}".encode('utf-8'))
+            data = game_connection.recv(1024)
+            if not data:
+                print("Server shutdown.")
+                break
+            game_connection.send(f"{msg}".encode('utf-8'))
     except Exception as e:
         print(f"Error occurred: {e}")
     finally:
         print("Disconnecting client...")
-        client_socket.close()
+        game_connection.close()
         print("Successfully disconnected.")
     
 
